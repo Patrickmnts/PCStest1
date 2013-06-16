cf385c568fca08b2e69c47e75cfcc078c194c8a3
Patrickmnts <patrickmnts@gmail.com>
2013-06-16 13:58:20 -0700

    final

--- Week 1/twixt.rb
+++ Week 1/twixt.rb
@@ -1,21 +1,22 @@
 class Twixt
+  def self.shout(name, last)
+    return name + last + "Woo Woo!"
+  end
+
   def initialize(pound)
     @pound = pound
   end
 
   def twist
-    return shout
+    return Twixt.shout
   end
 
   def pound
     @pound += 1
     return @pound
   end
-
-  def self.shout(name, last)
-    return name + last + "Woo Woo!"
-  end
 end
 
 puts Twixt.shout('Patrick ', 'Mounts ')
 puts Twixt.new(12).pound
+puts
